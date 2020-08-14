d78f42549627564b4314be4034b67fb03b159967
Jonathan Gervais <jgervais@baselinetelematics.com>
2020-08-14 14:17:36 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6d0798586a03d8904b17ea78fcff7e8c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '23009c64-1cd8-44b3-b0d6-767f5c0cea79'
+};
